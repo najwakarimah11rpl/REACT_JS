6f2b63d5a9f732cd4597823a1ce9aaf5a98e8cd8
NajwaKarimahN <147923660+najwakarimah11rpl@users.noreply.github.com>
2024-03-15 09:13:20 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
 function App(){
-    return (
-      <div className="App">
-        <h1>kontak smkrevit</h1>
-        <p>sidoarjo</p>
-      </div>
-    );
-  }
-  export default kontak;
+return (
+<div className="App">
+    <h1>kontak smkrevit</h1>
+    <p>sidoarjo</p> </div>);}
+export default kontak;

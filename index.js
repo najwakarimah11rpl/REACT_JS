6adfe8b9c7ad539805cc46b74429775986e2b40c
NajwaKarimahN <147923660+najwakarimah11rpl@users.noreply.github.com>
2024-03-15 09:18:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,10 @@ import Kontak from './pages/kontak';
 import Nav from './pages/nav';
 import Sejarah from './pages/sejarah';
 import reportWebVitals from 'reportWebVitals';
-
 ReactDOM.render(
     <React.StrictMode>
         <App />
         <Kontak />
     </React.StrictMode>
-    document.getElementById('root')
-);
-
-
-
+    document.getElementById('root'));
 reportWebVitals();
-
